8e139bdecd5cca7f8806841888dacdf6aad9140c
Jeremie <trulyepicman2@gmail.com>
2020-02-17 00:12:20 -0500

    intaverage

---
+++ scIntAverage.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class scIntAverage{
+
+  public static void main(String[] args){
+
+    int n1,n2,n3,n4,average;
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please state 4 whole numbers you wish for us to make an average of.");
+
+    n1 = sc.nextInt();
+    n2 = sc.nextInt();
+    n3 = sc.nextInt();
+    n4 = sc.nextInt();
+    average = (n1+n2+n3+n4)/4;
+
+    System.out.println("The average of " + n1 + "," + n2 + "," + n3 + "," + n4 + " is " + average + ".");
+
+
+  }
+
+}
