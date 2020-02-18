482ba0ee9a0c3db31db63b4a3db3714aa7adc48a
neighboornextdoor <2222@gmali.com>
2020-02-18 09:55:44 -0500

    Body Mass Index

---
+++ BMI.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class BMI{
+
+	public static void main(String[] args){
+
+	    Scanner sc = new Scanner(System.in);
+
+	    System.out.println("Please state your weight in pounds and height in feet in order for us to caluclate your BMI.");
+	    float weightp = sc.nextFloat();
+	    float heightf = sc.nextFloat();
+
+	    float weightkg = weightp*(0.453592f);
+	    float heightm = heightf*(0.3048f);
+
+	    System.out.printf("Your BMI is = " + (weightkg/(heightm*heightm)));
+    }
+}
