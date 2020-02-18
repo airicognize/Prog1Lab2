b826271e8c72dee05018a09dca48beab2ea67efc
neighboornextdoor <2222@gmali.com>
2020-02-18 10:21:01 -0500

    time

---
+++ time.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class time{
+
+	public static void main(String[] args){
+
+	    Scanner sc = new Scanner(System.in);
+	    
+	    System.out.print("Please state seconds you wish to convert>");
+        int seconds = sc.nextInt();
+        double minutes = seconds/60;
+        int secondleft = seconds%60;
+
+        System.out.print((int)minutes + ":" + secondleft + ".");
+    }
+}
