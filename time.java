2f963cb198de7e1058e1847d7f41b06a3f205b86
airi <59892547+airicognize@users.noreply.github.com>
2020-02-18 10:42:38 -0500

    Update time.java

--- time.java
+++ time.java
@@ -4,9 +4,9 @@ public class time{
 
 	public static void main(String[] args){
 
-	    Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	    
-	    System.out.print("Please state seconds you wish to convert>");
+	System.out.print("Please state seconds you wish to convert>");
         int seconds = sc.nextInt();
         double minutes = seconds/60;
         int secondleft = seconds%60;
